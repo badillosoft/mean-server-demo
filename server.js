af3c11c279b47c2ebc8dab20bc8e2f43b6b2e52f
Alan Badillo <badillo.soft@hotmail.com>
2018-05-24 15:43:46 -0500

    sesión 9

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hola heroku");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+http.createServer(app).listen(port, () => {
+    console.log(`Server started at http://localhost:${port}/`);
+});
