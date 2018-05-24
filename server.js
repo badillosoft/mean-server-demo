f0703bcdd2645ac49a4a319632a80cd5cfcdae97
Alan Badillo <badillo.soft@hotmail.com>
2018-05-24 15:45:23 -0500

    sesión 9

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>Holaaaaa</h1>
+    `);
+});
+
 const port = process.env.PORT || 3000;
 
 http.createServer(app).listen(port, () => {
